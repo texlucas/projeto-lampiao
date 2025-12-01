ba5e1b7e53e9038f7406b29ef8d784a95329bc4d
Abner do Nascimento <abnerdonantos06@gmail.com>
2025-12-01 12:21:02 -0300

    feat: add register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Button } from "@/components/ui/button"
 import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
 
   return (
     <>
       <div>
-        <Login />
+        <Register />
       </div>
     </>
   )
