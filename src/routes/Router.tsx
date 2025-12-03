c42d168e37cf7c30ac57b1820cfb2fd1e99a3549
Abner do Nascimento <abnerdonantos06@gmail.com>
2025-12-03 16:57:31 -0300

    feat: add routes

---
+++ src/routes/Router.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
+import Formulario from "../pages/Formulario/Formulario";
+
+const Router = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/formulario" element={<Home />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Router
